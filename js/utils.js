542fb8f47d51f1e759a6908676c9ac17bebf9f44
wenxiaoning <wenxiaoning@gochinatv.com>
2017-08-05 15:41:00 +0800

    增加联系页面

--- js/utils.js
+++ js/utils.js
@@ -3,6 +3,10 @@
  */
 'use strict';
 
+$(function () {
+    layout.init_html();
+})
+
 var utils = {
     isEmpty(val){
         if (val == '' || val == null || val == undefined || val == "undefined") {
@@ -37,14 +41,21 @@ var utils = {
 
 var layout = {
     window_adaptive(){
-        console.log(document.body.clientWidth, document.body.clientHeight)
-        console.log('screen', window.screen.width, window.screen.height)
-        console.log('scroll', document.body.scrollWidth, document.body.scrollHeight)
+        console.log(document.body.clientWidth, document.body.clientHeight);
+        console.log('screen', window.screen.width, window.screen.height);
+        console.log('scroll', document.body.scrollWidth, document.body.scrollHeight);
         var w = document.body.scrollWidth;
         var h = document.body.scrollHeight;
         // if (w <= h) {
         //     $(".layout").css({width: w})
         // }
+    },
+    init_html(){
+        $("#header").html('<span>wxnacy博客</span><nav><ul><li><a href="/">文章</a></li><li><a href="/pages/contact.html">联系</a></li></ul></nav>');
+        $("#footer").html(' © 2017 wxnacy.com 版权所有 <a href="http://www.miitbeian.gov.cn/" target="_blank">京ICP备15062634号-3</a>');
+        // var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+        // $('body').append(unescape("%3Cspan id='cnzz_stat_icon_1263285686'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s13.cnzz.com/z_stat.php%3Fid%3D1263285686%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"))
+        // document.write(unescape("%3Cspan id='cnzz_stat_icon_1263285686'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s13.cnzz.com/z_stat.php%3Fid%3D1263285686%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));
     }
 };
 
