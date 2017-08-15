114079ed6561cedc6b721e192c5e517f50822684
wenxiaoning <wenxiaoning@gochinatv.com>
2017-08-15 21:28:14 +0800

    正式发布第一版

--- run.py
+++ run.py
@@ -39,7 +39,7 @@ def before_request():
 
     logger.debug(g.ip)
 
-    CATEGORYS = ['python', 'mysql', 'git', 'ansible', 'js', 'mac', 'algorithm']
+    CATEGORYS = ['python', 'mysql', 'git', 'ansible', 'js', 'mac', 'algorithm','linux']
     file_list = os.listdir(BaseConfig.ARTICLE_DIR)
     categorys = set([o.split('-', 1)[0] for o in file_list])
     categorys = list(filter(lambda x: x in CATEGORYS, categorys))
