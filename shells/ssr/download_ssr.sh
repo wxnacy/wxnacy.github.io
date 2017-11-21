fc69a3bb5f9fee9e7b03e9c99e79b8a8bea031a8
wenxiaoning <wenxiaoning@gochinatv.com>
2017-11-21 11:14:16 +0800

    add-install-ssr4

--- shells/ssr/download_ssr.sh
+++ shells/ssr/download_ssr.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/ssr.sh && chmod +x ssr.sh
 wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh && chmod +x bbr.sh
-# yum install expect
-wget --no-check-certificate https://raw.githubusercontent.com/wxnacy/wxnacy.github.io/master/shells/ssr/install_ssr && chmod +x install_ssr
+yum -y install expect
+wget --no-check-certificate https://raw.githubusercontent.com/wxnacy/wxnacy.github.io/master/shells/ssr/install_ssr && chmod +x install_ssr && ./install_ssr
 
 # wget --no-check-certificate https://raw.githubusercontent.com/wxnacy/wxnacy.github.io/master/shells/ssr/download_ssr.sh && chmod +x download_ssr.sh && ./download_ssr.sh
-
