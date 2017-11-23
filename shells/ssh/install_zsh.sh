c997d2838b82fd123f97747f6c3b822328ea8c31
wenxiaoning <wenxiaoning@gochinatv.com>
2017-11-23 17:44:59 +0800

    add-install-zsh

--- shells/ssh/install_zsh.sh
+++ shells/ssh/install_zsh.sh
@@ -1,26 +1,33 @@
 #!/usr/bin/env bash
+
+# wget https://raw.githubusercontent.com/wxnacy/wxnacy.github.io/master/shells/ssh/install_zsh.sh && chmod +x install_zsh.sh && ./install_zsh.sh
+
 sysOS=`uname -s`
 
 update_zshrc(){
-    echo 'source ~/.bash_profile' >> $1
-    echo 'source $ZSH/oh-my-zsh.sh' >> $1
-    echo 'export ZSH=${HOME}/.oh-my-zsh' >> $1
-    echo '' >> $1
-    echo 'ZSH_THEME="fishy"' >> $1
-    echo 'plugins=(git python sublime)' >> $1
-    echo '' >> $1
-    echo 'alias zshc="vim ~/.zshrc"' >> $1
-    echo 'alias zshs="source ~/.zshrc"' >> $1
-    echo 'alias vimc="vim ~/.vimrc"' >> $1
-    echo 'alias bp="vim ~/.bash_profile"' >> $1
-    echo 'alias bps="source ~/.bash_profile"' >> $1
-    echo 'alias sshc="vim ~/.ssh/config"' >> $1
-    echo 'alias c="clear"' >> $1
-    echo 'alias h="history"' >> $1
+    sudo mv ~/.zshrc ~/.zshrc_old
+
+    sudo echo 'source ~/.bash_profile' >> $1
+    sudo echo 'source $ZSH/oh-my-zsh.sh' >> $1
+    sudo echo 'export ZSH=${HOME}/.oh-my-zsh' >> $1
+    sudo echo '' >> $1
+    sudo echo 'ZSH_THEME="fishy"' >> $1
+    sudo echo 'plugins=(git python sublime)' >> $1
+    sudo echo '' >> $1
+    sudo echo 'alias zshc="vim ~/.zshrc"' >> $1
+    sudo echo 'alias zshs="source ~/.zshrc"' >> $1
+    sudo echo 'alias vimc="vim ~/.vimrc"' >> $1
+    sudo echo 'alias bp="vim ~/.bash_profile"' >> $1
+    sudo echo 'alias bps="source ~/.bash_profile"' >> $1
+    sudo echo 'alias sshc="vim ~/.ssh/config"' >> $1
+    sudo echo 'alias c="clear"' >> $1
+    sudo echo 'alias h="history"' >> $1
+
     source ~/.zshrc
 }
 
 install_ubuntu(){
+    echo 'I am ubuntu'
     echo 'begin apt-get update'
     sudo apt-get update
     echo 'begin install git'
@@ -32,26 +39,6 @@ install_ubuntu(){
     update_zshrc ~/.zshrc
     zsh
 }
-ZSHRC='
-source ~/.bash_profile
-export ZSH=${HOME}/.oh-my-zsh
-
-
-ZSH_THEME="fishy"
-
-plugins=(git python sublime)
-source $ZSH/oh-my-zsh.sh
-
-alias zshc="vim ~/.zshrc"
-alias zshs="source ~/.zshrc"
-alias vimc="vim ~/.vimrc"
-alias bp="vim ~/.bash_profile"
-alias bps="source ~/.bash_profile"
-alias sshc="vim ~/.ssh/config"
-
-alias c="clear"
-alias h="history"
-'
 main(){
     if [ $sysOS == "Darwin"  ];then
         echo "I'm MacOS"
