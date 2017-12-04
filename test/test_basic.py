d79a81130c26d1866a412253b8933ecfdc9613c4
wenxiaoning <wenxiaoning@gochinatv.com>
2017-12-04 22:13:42 +0800

    test

--- test/test_basic.py
+++ test/test_basic.py
@@ -3,7 +3,7 @@
 # Author: wxnacy(wxnacy@gmail.com)
 # Description:
 
-
+from datetime import datetime
 
 ex_list = [dict(age=44, n=74), dict(age=43, n=44), dict(age=24, n=43), 
         dict(age=74, n=24)]
@@ -12,10 +12,12 @@ l = ['BB', 'aa', 'cc']
 
 
 if __name__ == "__main__":
-     print(ex_list)
-     #  ex_list.sort(key=lambda x: x['age'])
-     #  ex_list.sort(key=age)
-     print(ex_list)
-     print(l)
-     l.sort(key=str.lower)
-     print(l)
+     #  print(ex_list)
+     #  #  ex_list.sort(key=lambda x: x['age'])
+     #  #  ex_list.sort(key=age)
+     #  print(ex_list)
+     #  print(l)
+     #  l.sort(key=str.lower)
+     #  print(l)
+     print(datetime.now().timestamp())
+     print(datetime.utcnow().timestamp())
