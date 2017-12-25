8bff70be82e4edb0e4d54e40117effd15cce4fca
wenxiaoning <wenxiaoning@gochinatv.com>
2017-12-25 22:03:51 +0800

    add

--- hexo/source/hot/index.md
+++ hexo/source/hot/index.md
@@ -7,9 +7,12 @@ date: 2017-12-22 14:18:55
 - [Vim 插件 YouCompleteMe 代码自动补全](/2017/09/22/vim-plugin-youcompleteme)
 - [Vim 插件 Ack 全局搜索文件](/2017/10/13/vim-plugin-ack)
 - [Vim 插件 ctrlp 模糊搜索文件](/2017/09/23/vim-plugin-ctrlp)
-- [Vim 插件 pathogen 插件管理](/2017/09/20/vim-plugin-pathogen)
+- [MD5，SHA，HMAC ，AES 在线加密计算](/2017/12/19/tool-crypto)
+- [Vim 插件 CtrlSF 全局搜索文件](/2017/10/13/vim-plugin-ctrlsf/)
 - [Vim 插件 vim-airline 状态栏增强显示](/2017/09/25/vim-plugin-artline)
+- [Vim 插件 pathogen 插件管理](/2017/09/20/vim-plugin-pathogen)
+- [Hexo 使用不蒜子计算访问量](/2017/09/18/busuanzi/)
 - [Vim 插件 EasyGrep 全局搜索文件](/2017/10/13/vim-plugin-easygrep)
-- [如何获取指定位数随机数](/2017/12/06/get-random/)
-- [获取时间戳及世界时间转换](/2017/12/09/timestamp/)
-- [网络请求断线重连工具 Mosh](/2017/09/25/mosh/)
+<!-- - [如何获取指定位数随机数](/2017/12/06/get-random/) -->
+<!-- - [获取时间戳及世界时间转换](/2017/12/09/timestamp/) -->
+<!-- - [网络请求断线重连工具 Mosh](/2017/09/25/mosh/) -->
--- test/test_utils.py
+++ test/test_utils.py
@@ -9,17 +9,54 @@ import os
 import sys
 import time
 from datetime import datetime
+import msgpack
+
+class User():
+    def __init__(self):
+        self.name = 'wxnacy'
+
+    def __str__(self):
+        return self.name
 
 if __name__ == "__main__":
-    print(time.time())
-    utc = time.gmtime(time.time())
-    print(utc)
-    res = datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d %H:%M:%S")
-    print(res)
-    t = '2017-12-10 16:37:17'
-    t = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
-    res = int(time.mktime(time.strptime(t, '%Y-%m-%d %H:%M:%S')))
-    print(res)
-    print(datetime.now().timestamp())
-    res = datetime.strptime(t, "%Y-%m-%d %H:%M:%S")
-    print(res)
+
+    #  print(time.time())
+    #  utc = time.gmtime(time.time())
+    #  print(utc)
+    #  res = datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d %H:%M:%S")
+    #  print(res)
+    #  t = '2017-12-10 16:37:17'
+    #  t = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
+    #  res = int(time.mktime(time.strptime(t, '%Y-%m-%d %H:%M:%S')))
+    #  print(res)
+    #  print(datetime.now().timestamp())
+    #  res = datetime.strptime(t, "%Y-%m-%d %H:%M:%S")
+    #  print(res)
+    #  u = User()
+    #  print(u)
+    #  res = msgpack.packb(u, use_bin_type=True)
+    #  print(res)
+    #  res = msgpack.unpackb(res, encoding='utf-8')
+    #  print(res)
+    useful_dict = {
+        "id": 1,
+        "created": datetime.now(),
+    }
+
+    def decode_datetime(obj):
+        if '__datetime__' in obj:
+            #  print(obj)
+            obj = datetime.strptime(obj["as_str"], "%Y%m%dT%H:%M:%S.%f")
+        return obj
+
+    def encode_datetime(obj):
+        if isinstance(obj, datetime):
+            return {'__datetime__': True, 'as_str': obj.strftime("%Y%m%dT%H:%M:%S.%f")}
+        return obj
+
+
+    packed_dict = msgpack.packb(useful_dict, default=encode_datetime)
+    print(packed_dict)
+    this_dict_again = msgpack.unpackb(packed_dict, object_hook=decode_datetime,
+            encoding='utf-8')
+    print(this_dict_again)
