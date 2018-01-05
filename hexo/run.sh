bdc65e5a088d471ff9d1f7a7bc12462b37a388e9
wenxiaoning <wenxiaoning@gochinatv.com>
2018-01-05 15:30:53 +0800

    add-art

--- hexo/run.sh
+++ hexo/run.sh
@@ -1,6 +1,6 @@
 kill -9 `ps aux | grep hexo| awk '{print $2}'`
 JS_HOME="themes/yilia/source/js"
 uglifyjs ${JS_HOME}/wxnacy.js -o ${JS_HOME}/wxnacy.min.js -m
-uglifyjs ${JS_HOME}/wxnacy.js -o ${JS_HOME}/wxnacy.is9dkehs.js -m
+uglifyjs ${JS_HOME}/wxnacy.js -o ${JS_HOME}/wxnacy.rjfiej8.js -m
 uglifyjs ${JS_HOME}/ts.js -o ${JS_HOME}/ts.min.js -m
 nohup hexo server --draft &
