2bbdcab07470b3f104bcf6c741ee68c3f3248b46
wxnacy <371032668@qq.com>
2018-05-18 14:59:48 +0800

    update

---
+++ shells/README.md
@@ -0,0 +1,9 @@
+# 各种自动化脚本
+
+## Linux
+
+**Linux 系统必备软件**
+
+```bash
+$ curl -L https://raw.githubusercontent.com/wxnacy/wxnacy.github.io/master/shells/linux/linux_need.sh | bash
+```
--- shells/linux/linux_need.sh
+++ shells/linux/linux_need.sh
@@ -1,3 +1,32 @@
 #!/usr/bin/bash
 
-echo $PATH
+PKG=''
+
+check_system(){
+    OS=`uname -s`
+    if [ ${OS} == "Darwin"  ];then
+        PKG='brew'
+    elif [ ${OS} == "Linux"  ];then
+        source /etc/os-release
+        case $ID in
+            debian|ubuntu|devuan)
+                PKG='apt-get'
+                ;;
+            centos|fedora|rhel)
+                PKG="yum"
+                if test "$(echo "$VERSION_ID >= 22" | bc)" -ne 0;
+                then
+                    PKG="dnf"
+                fi
+                ;;
+            *)
+                exit 1
+                ;;
+        esac
+    else
+        echo "Other OS: ${OS}"
+    fi
+}
+
+check_system
+echo $PKG
