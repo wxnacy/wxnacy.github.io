81b6ecd29755c5216cbc1c0037123abc3dd195ab
wxnacy <371032668@qq.com>
2018-05-23 10:35:07 +0800

    add

--- shells/linux/linux_need.sh
+++ shells/linux/linux_need.sh
@@ -35,19 +35,20 @@ check_system(){
 install(){
     sudo ${PKG} -y update
     sudo ${PKG} -y install gcc g++ make cmake
-    sudo ${PKG} -y install git vim wget
+    sudo ${PKG} -y install git vim wget unzip
     if [ ${SYS} == 'ubuntu' ]
     then
         sudo ${PKG} -y install htop python-pip
         sudo pip install httpie
         sudo ${PKG} -y install patch zlib1g.dev libgdbm-dev libssl-dev libsqlite3-dev libbz2-dev libreadline-dev
-        echo ${SYS}
+        sudo ${PKG} -y install dnsutils     # dig
     elif [ ${SYS} == 'centos' ]
     then
         sudo ${PKG} -y install gcc-c++ aclocal
         sudo ${PKG} -y install pcre pcre-devel openssl openssl-devel # nginx
         sudo ${PKG} -y install epel-release htop # htop need
         sudo ${PKG} -y install install zlib zlib-devel  # GeoIp need
+        sudo ${PKG} -y install install bind-utils       # dig
 
         sudo ${PKG} -y install readline readline-devel readline-static openssl-static sqlite-devel bzip2-devel bzip2-libs
     elif [ ${SYS} == 'mac' ]
