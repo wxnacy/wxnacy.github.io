10a2ddbe97c91ad2a1b044ec18783ec0dc4fba5c
wxnacy <371032668@qq.com>
2018-05-24 17:24:08 +0800

    add-server

--- shells/linux/send_server.sh
+++ shells/linux/send_server.sh
@@ -2,5 +2,12 @@
 
 IP=`curl ifconfig.io`
 ETCTIP=$1
-curl -X PUT http://${ETCTIP}:2379/v2/keys/upstreams/api/${IP}:8888?ttl=20 \
-    -d value="{\"server\": \"${IP}:8888\", \"weight\": 5}"
+PORT=$2
+
+if [ ! ${PORT} ]
+then
+    PORT=8888
+fi
+
+curl -X PUT http://${ETCTIP}:2379/v2/keys/upstreams/api/${IP}:${PORT}?ttl=65 \
+    -d value="{\"server\": \"${IP}:${PORT}\", \"weight\": 5}"
