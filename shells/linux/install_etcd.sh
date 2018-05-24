de3585eebd35bd91dfaa7b6030d03afe2788caca
wxnacy <371032668@qq.com>
2018-05-24 14:43:15 +0800

    add-initd

--- shells/linux/install_etcd.sh
+++ shells/linux/install_etcd.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-sudo yum update -y
-sudo yum install -y wget
 
-wget https://github.com/coreos/etcd/releases/download/v3.3.5/etcd-v3.3.5-linux-amd64.tar.gz
+curl -o etcd-v3.3.5-linux-amd64.tar.gz https://github.com/coreos/etcd/releases/download/v3.3.5/etcd-v3.3.5-linux-amd64.tar.gz
 tar -zxvf etcd-v3.3.5-linux-amd64.tar.gz
 sudo cp etcd-v3.3.5-linux-amd64/etcd* /usr/bin/
 
