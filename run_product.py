f0a0999447eebadce59b674a30c0357ca6571668
wxnacy <371032668@qq.com>
2018-07-19 16:47:00 +0800

    add-wx

---
+++ run_product.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''配置信息'''
+__author__ = "wxnacy(wxnacy@gmail.com)"
+__copyright__ = "Copyright of wxnacy (2017)."
+
+from app.run import app
+
+if __name__ == "__main__":
+    app.run(debug=False, port=4010)
