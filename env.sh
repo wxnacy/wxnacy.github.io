d3829920fdd8658e96c5ba49f0c9fda99a584d23
wxnacy <371032668@qq.com>
2019-04-19 09:42:14 +0800

    add

--- env.sh
+++ env.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-ENV=$1
+local_env=$1
 
-if [ ! ${ENV} ]
+if [ ! ${local_env} ]
 then
-    ENV=local
+    local_env=local
 fi
 
 export PYTHONPATH=`pwd` # 项目跟目录
-export FLASK_CONFIG=${ENV} # 当前环境 可选 local product dev test
+export FLASK_CONFIG=${local_env} # 当前环境 可选 local product dev test
 export NODE_PATH=`pwd`/nodejs
